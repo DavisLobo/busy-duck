e4859e31b111dfe3f4b3f85f8ddc4339e2119313
DavisLobo <davisrlobos@gmail.com>
2026-08-20 15:55:46 -0300

    Setup event data model

---
+++ src/busy_duck/models/event.py
@@ -0,0 +1,21 @@
+# Event
+
+# Business Fields
+# ---------------
+# id
+# title
+# description
+# start_datetime
+# end_datetime
+# location
+
+# Synchronization Fields
+# ----------------------
+# provider_id
+# account_id
+# external_id
+
+# Metadata
+# --------
+# created_at
+# updated_at
