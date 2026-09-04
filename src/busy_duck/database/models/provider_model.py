2235d747d54a3447bc56243d711d0f3e4ebf8c1a
DavisLobo <davisrlobos@gmail.com>
2026-09-04 15:42:48 -0300

    Setup provider_model for datetime integrations

--- src/busy_duck/database/models/provider_model.py
+++ src/busy_duck/database/models/provider_model.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from uuid import uuid4
 
 from sqlalchemy import DateTime, String
@@ -7,14 +7,23 @@ from sqlalchemy.orm import Mapped, mapped_column
 from busy_duck.database.models.base import Base
 
 
-
 class ProviderModel(Base):
     __tablename__ = "providers"
-    
-    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid4()))
-    
-    name: Mapped[str] = mapped_column(String(100))
-    slug: Mapped[str] = mapped_column(String(50), unique=True)
-    
-    created_at: Mapped[datetime] = mapped_column(DateTime)
-    updated_at: Mapped[datetime] = mapped_column(DateTime)
+
+    id: Mapped[str] = mapped_column(
+        String(36),
+        primary_key=True,
+        default=lambda: str(uuid4()),
+    )
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    slug: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
